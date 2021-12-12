20a0aef51e75155bafd0a62290e5d27123e2befa
Kien Nguyen The <cudayanh@gmail.com>
2021-12-13 01:12:00 +0900

    fixed port

--- index.js
+++ index.js
@@ -69,9 +69,13 @@ console.log(`node env: ${process.env.NODE_ENV}`)
 
 if (process.env.NODE_ENV !== "development") {
 	app.use(express.static("client/build")) // serve the static react app
-	app.get(/^\/(?!api).*/, (req, res) => {
+	app.use(express.static(path.join(__dirname, "client/build")))
+	app.get("*", (req, res) => {
 		// don't serve api routes to react app
 		// res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+		// app.get("*", (req, res) => {
+		// 	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+		// })
 		res.sendFile(path.join(__dirname, "/client/build/index.html"))
 	})
 	console.log("Serving React App...")
